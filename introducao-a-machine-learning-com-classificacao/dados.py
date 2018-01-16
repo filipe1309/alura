da89831b998a99cb0d19b7c638e57aaefed407de
filipe1309 <filipe1309@gmail.com>
2018-01-15 23:26:53 -0200

    Implement a new CSV reader
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- introducao-a-machine-learning-com-classificacao/dados.py
+++ introducao-a-machine-learning-com-classificacao/dados.py
@@ -18,4 +18,20 @@ def carregar_acessos():
 
 	return X, Y
 
+def carregar_buscas():
+	
+	# dados
+	X = []
+	# marcacoes
+	Y = []
 
+	arquivo = open('busca.csv', 'rb')
+	leitor = csv.reader(arquivo)
+	leitor.next() # pula a primeira linha
+	for home,busca,logado,comprou in leitor:
+
+		dado = [int(home), busca, int(logado)]
+		X.append(dado)
+		Y.append(int(comprou))
+
+	return X, Y
