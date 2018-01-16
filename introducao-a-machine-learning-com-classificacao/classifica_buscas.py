facf502dd3fe6e8f798f91a7df9ece5833e110e4
filipe1309 <filipe1309@gmail.com>
2018-01-15 23:38:32 -0200

    Replace CSV reader with Pandas data analysis library solution using data frame object
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- introducao-a-machine-learning-com-classificacao/classifica_buscas.py
+++ introducao-a-machine-learning-com-classificacao/classifica_buscas.py
@@ -1,4 +1,5 @@
-from dados import carregar_buscas
-
-X,Y = carregar_buscas();
-print(X)
+# using panda data analysis - sudo pip install pandas --upgrade
+import pandas as pd
+# data frame
+df = pd.read_csv('busca.csv')
+print(df)
