798e134ea3b59371ea9211743a1d04a30ebb7867
filipe1309 <filipe1309@gmail.com>
2018-01-16 00:13:36 -0200

    Implement Naive Bayes from sckit-learn lib
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- introducao-a-machine-learning-com-classificacao/classifica_buscas.py
+++ introducao-a-machine-learning-com-classificacao/classifica_buscas.py
@@ -24,6 +24,17 @@ teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
 
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+modelo.fit(treino_dados, treino_marcacoes)
 
-print(len(treino_marcacoes))
-print(len(teste_marcacoes))
+resultado = modelo.predict(teste_dados)
+diferencas = resultado - teste_marcacoes
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste_dados)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(taxa_de_acerto)
+print(total_de_elementos)
