46070fc45b429e4784c56192b98b6823a7231835
filipe1309 <filipe1309@gmail.com>
2018-01-17 22:26:17 -0200

    Change base algorithm to use the same data that the implemented naive beyes algorithm uses
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- introducao-a-machine-learning-com-classificacao/classifica_buscas.py
+++ introducao-a-machine-learning-com-classificacao/classifica_buscas.py
@@ -14,13 +14,6 @@ Ydummies_df = Y_df;
 X = Xdummies_df.values
 Y = Ydummies_df.values
 
-# a eficacia do algoritmo que chuta tudo 0 ou 1
-acerto_base = max(Counter(Y).itervalues())
-# acerto_de_um = list(Y).count('sim') # len(Y[Y==1]) # sum(Y)
-# acerto_de_zero = list(Y).count('nao') # len(Y[Y==0]) # len(Y) - acerto_de_um
-taxa_de_acerto_base = 100.0 * acerto_base / len(Y)
-print("Taxa de acerto base: %f" % taxa_de_acerto_base)
-
 # 90% train, 10% test
 porcentagem_de_treino = 0.9
 tamanho_de_treino = int(porcentagem_de_treino * len(Y))
@@ -48,3 +41,11 @@ taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
 print("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
 print(total_de_elementos)
+
+
+# a eficacia do algoritmo que chuta tudo um unico valor
+acerto_base = max(Counter(teste_marcacoes).itervalues())
+# acerto_de_um = list(Y).count('sim') # len(Y[Y==1]) # sum(Y)
+# acerto_de_zero = list(Y).count('nao') # len(Y[Y==0]) # len(Y) - acerto_de_um
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
+print("Taxa de acerto base: %f" % taxa_de_acerto_base)
