501eff0fdae929aa005df6e990470fc893227d31
filipe1309 <filipe1309@gmail.com>
2018-01-22 22:21:08 -0200

    Compare solution of AdaBoostClassifier and MultinomialNB algorithms
    Signed-off-by: filipe1309 <filipe1309@gmail.com>

--- introducao-a-machine-learning-com-classificacao/classifica_buscas.py
+++ introducao-a-machine-learning-com-classificacao/classifica_buscas.py
@@ -10,7 +10,7 @@ import pandas as pd
 # busca => 85.71% (7 tests)
 
 # data frame
-df = pd.read_csv('busca.csv')
+df = pd.read_csv('busca2.csv')
 
 X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
@@ -33,25 +33,29 @@ teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
 
-#from sklearn.naive_bayes import MultinomialNB
-#modelo = MultinomialNB()
+def fit_and_predict(nome, modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes):
+	modelo.fit(treino_dados, treino_marcacoes)
+
+	resultado = modelo.predict(teste_dados)
+	acertos = (resultado == teste_marcacoes)
+	# diferencas = resultado - teste_marcacoes
+
+	# acertos = [d for d in diferencas if d == 0]
+	total_de_acertos = sum(acertos)
+	total_de_elementos = len(teste_dados)
+	taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+	msg = "Taxa de acerto do algoritmo: {0}: {1}".format(nome, taxa_de_acerto)
+	print(msg)
+
+
+from sklearn.naive_bayes import MultinomialNB
+modelo = MultinomialNB()
+fit_and_predict("MultinomialNB", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
 from sklearn.ensemble import AdaBoostClassifier
 modelo = AdaBoostClassifier()
-
-modelo.fit(treino_dados, treino_marcacoes)
-
-resultado = modelo.predict(teste_dados)
-acertos = (resultado == teste_marcacoes)
-# diferencas = resultado - teste_marcacoes
-
-# acertos = [d for d in diferencas if d == 0]
-total_de_acertos = sum(acertos)
-total_de_elementos = len(teste_dados)
-taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
-
-print("Taxa de acerto do algoritmo: %f" % taxa_de_acerto)
-print(total_de_elementos)
+fit_and_predict("AdaBoostClassifier", modelo, treino_dados, treino_marcacoes, teste_dados, teste_marcacoes)
 
 
 # a eficacia do algoritmo que chuta tudo um unico valor
@@ -60,3 +64,4 @@ acerto_base = max(Counter(teste_marcacoes).itervalues())
 # acerto_de_zero = list(Y).count('nao') # len(Y[Y==0]) # len(Y) - acerto_de_um
 taxa_de_acerto_base = 100.0 * acerto_base / len(teste_marcacoes)
 print("Taxa de acerto base: %f" % taxa_de_acerto_base)
+print("Total de testes %d" % len(teste_dados))
